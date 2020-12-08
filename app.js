73dd2566806951b53d7c8591dd92506b22f4bc98
abhishek_rawe <65603830+abhishekrawe@users.noreply.github.com>
2020-12-08 06:16:08 -0800

    login_page_serve_side

---
+++ app.js
@@ -0,0 +1,17 @@
+require("dotnev").config();
+const express =  require("express");
+const app = express();
+const userRouter = require("./api/users/user.router");
+
+app.use("/api/users", userRouter);
+
+
+// app.get("/api", (req,res) => {
+//     res.json({
+//         success: "This is rest apis working"
+//     });
+// });
+
+app.listen(process.env.APP_PORT,()=>{
+    console.log("Server up and running on PORT : " , process.env.APP_PORT);
+});
